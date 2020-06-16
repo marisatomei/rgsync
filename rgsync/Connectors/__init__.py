4ab0311d1e94b7a49122a90b2eb5c3eb548d96cf
GONZALEZ Marcelino <Y0772666@arcelormittal.es>
2020-06-16 17:54:15 +0200

    test

--- rgsync/Connectors/__init__.py
+++ rgsync/Connectors/__init__.py
@@ -1,17 +1,17 @@
 from .simple_hash_connector import SimpleHashConnector
-from .sql_connectors import MySqlConnector, SQLiteConnection, OracleSqlConnector,SnowflakeSqlConnector,MySqlConnection,OracleSqlConnection,SnowflakeSqlConnection,SQLiteConnector
+from .sql_connectors import MySqlConnector, SQLiteConnection, OracleSqlConnector,SnowflakeSqlConnector,MySqlConnection,OracleSqlConnection,SnowflakeSqlConnection,SQLiteConnector,InfluxDbConnector,InfluxDbConnection
 from .cql_connector import CqlConnector, CqlConnection
 
 __all__ = [
     'SimpleHashConnector',
-
+    'InfluxDbConnection',
     'MySqlConnector',
     'OracleSqlConnector',
     'SnowflakeSqlConnector',
     'MySqlConnection',
     'OracleSqlConnection',
     'SnowflakeSqlConnection',
-
+    'InfluxDbConnector',
     'CqlConnector',
     'CqlConnection'
 ]
